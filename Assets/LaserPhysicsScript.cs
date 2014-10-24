c0560ed3eb73e6e18955c31a0af6298a22dc6413
M D <dudecrushman@gmail.com>
2014-10-23 18:54:03 -0700

    Added laser path auto correct on reflect. Currently set to 300 degrees tolerance (silly, but demonstrates functionality).

--- Assets/LaserPhysicsScript.cs
+++ Assets/LaserPhysicsScript.cs
@@ -7,6 +7,7 @@ public class LaserPhysicsScript : MonoBehaviour
 
     public float hitPlayerMagnitude = 400.0f;
     public float speed = 0.25f;
+    public float autoCorrectDegrees = 10.0f;
 
     // Use this for initialization
     void Start()
@@ -36,19 +37,7 @@ public class LaserPhysicsScript : MonoBehaviour
         }
 		else if(collision.gameObject.name.Contains("Reflect") || collision.gameObject.tag.Contains("Reflect"))
         {
-            Heading = Vector3.Reflect(Heading, collision.contacts[0].normal);
-
-            float angle = Mathf.Atan(Heading.y / Heading.x);
-            angle *= (180.0f / Mathf.PI);
-            angle -= 90.0f;
-
-            if (Heading.x < 0)
-            {
-				angle += 180.0f;
-            }
-
-            Quaternion laserRotation = Quaternion.AngleAxis(angle, new Vector3(0, 0, 1));
-            transform.rotation = laserRotation;
+            OnHitReflect(collision);
         }
         else if (collision.gameObject.name.Equals("Ball"))
         {
@@ -64,5 +53,163 @@ public class LaserPhysicsScript : MonoBehaviour
             GameObject.Find("Ball").rigidbody2D.AddForce(Heading.normalized * hitPlayerMagnitude);
             Destroy(gameObject);
         }
+    }
+
+    float SpeedPerSecond()
+    {
+        return this.speed * (float)(1.0 / Time.deltaTime);
+    }
+
+    float LaserHitParabolaTime(Vector2 ballPos, Vector2 ballVelocity, Vector2 laserStart, float ballTime)
+    {
+        float hitX = ballPos.x + ballVelocity.x * ballTime;
+
+        // d = vt + 0.5at^2
+        float hitY = ballPos.y + ballVelocity.y * ballTime + 0.5f * -9.8f * (ballTime * ballTime);
+
+        float deltaX = hitX - laserStart.x;
+        float deltaY = hitY - laserStart.y;
+
+        float distanceToHit = Mathf.Sqrt(deltaX * deltaX + deltaY * deltaY);
+
+        float timeToHit = distanceToHit / SpeedPerSecond();
+
+        return timeToHit;
+    }
+
+    float DeltaAngleToHitBall(Vector2 ballPos, Vector2 ballVelocity, Vector2 laserStart, float currentAngle)
+    {
+        int iterationLimit = 100;
+        int iteration = 0;
+
+        // first find an upper bound, a time such that the laser will intersect the parabola at a point
+        // before the ball reaches it
+        float upperTime = 0.0f;
+
+        for (iteration = 0; iteration < iterationLimit; iteration++)
+        {
+            float laserTime = LaserHitParabolaTime(ballPos, ballVelocity, laserStart, upperTime);
+            
+            if (laserTime < upperTime)
+            {
+                break;
+            }
+
+            upperTime += 0.1f;
+        }
+
+        if(iteration == iterationLimit)
+        {
+            Debug.Log("No Solution");
+            return 0;
+        }
+
+        // zero will be a fine lower bound, as the ball will always reach the point it is already at before
+        // the laser gets there
+        float lowerTime = 0.0f;
+
+        // now binary search between lower time and upper time until the ball and the laser time are within
+        // a small delta
+        float acceptableDelta = 0.05f;
+
+        // binary search result
+        float foundTime = 0.0f;
+
+        for (iteration = 0; iteration < iterationLimit; iteration++)
+        {
+            float halfTime = (upperTime + lowerTime) / 2;
+
+            float laserTime = LaserHitParabolaTime(ballPos, ballVelocity, laserStart, halfTime);
+
+            if (Mathf.Abs(halfTime - laserTime) < acceptableDelta)
+            {
+                foundTime = halfTime;
+                break;
+            }
+            else if (laserTime < halfTime)
+            {
+                // laser will get to point first, pass ball ahead of it, move upper down
+                upperTime = halfTime;
+            }
+            else
+            {
+                // ball will get to point first, laser will pass ball behind it, move lower up
+                lowerTime = halfTime;
+            }
+        }
+
+        if (iteration == iterationLimit)
+        {
+            Debug.Log("No Solution");
+            return 0;
+        }
+
+        float hitX = ballPos.x + ballVelocity.x * foundTime;
+        // d = vt + 0.5at^2
+        float hitY = ballPos.y + ballVelocity.y * foundTime + 0.5f * -9.8f * (foundTime * foundTime);
+
+        // the decided path line
+        //Debug.DrawLine(laserStart, new Vector2(hitX, hitY), Color.cyan, 2.0f);
+
+        float deltaX = hitX - laserStart.x;
+        float deltaY = hitY - laserStart.y;
+
+        float angle = Mathf.Atan(deltaY / deltaX);
+        angle *= (180.0f / Mathf.PI);
+
+        if (deltaX < 0)
+        {
+            angle += 180.0f;
+        }
+
+        return angle - currentAngle;
+    }
+
+    void OnHitReflect(Collision2D collision)
+    {
+        Rigidbody2D ball = GameObject.Find("Ball").rigidbody2D;
+
+        // the ball parabola
+        //for (float ti = ball.position.x - 5; ti < ball.position.x + 15; ti += 0.1f)
+        //{
+        //    Vector2 doop = new Vector2(ti,
+        //                               ball.velocity.y * (ti - ball.position.x) / ball.velocity.x +
+        //                               0.5f * (-9.8f) * ((ti - ball.position.x) / ball.velocity.x) * ((ti - ball.position.x) / ball.velocity.x) + ball.position.y);
+        //    ti = ti + 0.1f;
+        //    Vector2 dooop = new Vector2(ti,
+        //                                ball.velocity.y * (ti - ball.position.x) / ball.velocity.x +
+        //                                0.5f * (-9.8f) * ((ti - ball.position.x) / ball.velocity.x) * ((ti - ball.position.x) / ball.velocity.x) + ball.position.y);
+
+        //    Debug.DrawLine(doop, dooop, Color.red, 2);
+
+        //    ti = ti - 0.1f;
+        //}
+
+        Heading = Vector3.Reflect(Heading, collision.contacts[0].normal);
+
+        float angle = Mathf.Atan(Heading.y / Heading.x);
+        angle *= (180.0f / Mathf.PI);
+        angle -= 90.0f;
+
+        if (Heading.x < 0)
+        {
+            angle += 180.0f;
+        }
+
+        // correction
+        float deltaAngle = DeltaAngleToHitBall(ball.position, ball.velocity, gameObject.transform.position, angle);
+
+        if(Mathf.Abs(deltaAngle) < autoCorrectDegrees)
+        {
+            angle += deltaAngle;
+            Heading = Quaternion.AngleAxis(deltaAngle - 90.0f, new Vector3(0, 0, 1)) * Heading;
+            angle += 90.0f;
+        }
+
+        Quaternion laserRotation = Quaternion.AngleAxis(angle, new Vector3(0, 0, 1));
+        transform.rotation = laserRotation;
+
+        // the path taken line
+        //Debug.DrawLine(transform.position, transform.position + 40.0f * Heading, Color.red, 2);
     }
 }
